c7eeaf4fc24a7cb4f0be77f85bcb57599b8b353d
EC2 Default User <ec2-user@ip-172-31-2-191.ec2.internal>
2021-11-17 05:04:04 +0000

    added test file

--- test_main.py
+++ test_main.py
@@ -1,3 +1,24 @@
 import requests
 
-message = 
+def Ping():
+    
+    sunsigns = ["aries", "taurus", "gemini", "cancer", "leo", "virgo",
+           "libra", "scorpio", "sagittarius", "capricorn", "aquarius", "pisces"]
+    timeframe = ["today", "week", "month", "year"]
+    
+    for time in timeframe:
+        for sign in sunsigns:
+            horo = f"http://horoscope-api.herokuapp.com/horoscope/{time}/{sign}"
+            response = requests.get(horo)
+            
+            try:
+                return response.json()["horoscope"]
+                    
+            except ValueError:
+                return "Link Not Found"
+                
+def test_link_check():
+    assert Ping() != "Link Not Found"
+
+if __name__ == "__main__":
+    print(test_link_check())
